fba637244b9ab6b4349933e79f5f9156f3928361
Punit Sukhani <sukhanipunit@gmail.com>
2025-06-23 18:31:20 +0530

    basic frontend and backend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import { Homepage } from './Pages/Homepage'
-import { ChatPage } from './Pages/ChatPage'
+import { Routes, Route } from 'react-router-dom';
+import { Homepage } from './Pages/Homepage';
+import { ChatPage } from './Pages/ChatPage';
 
 function App() {
   return (
-    <div className='App'>
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/chats' element={<ChatPage />} />
-      </Routes>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/chats" element={<ChatPage />} />
+    </Routes>
+  );
 }
-export default App 
+
+export default App;
